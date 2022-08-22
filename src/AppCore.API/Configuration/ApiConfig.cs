8db6bb27e7f17e686d0f102b393b23cccf009aab
Mateus Castellar <mateuscastellar@outlook.com>
2022-08-22 12:36:03 -0300

    refactor: Clen Code startup com metodos de extensoes

---
+++ src/AppCore.API/Configuration/ApiConfig.cs
@@ -0,0 +1,51 @@
+﻿using AppCore.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+namespace AppCore.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, string connectionString)
+        {
+            services.Configure<ApiBehaviorOptions>(lbda =>
+            {
+                lbda.SuppressModelStateInvalidFilter = true;
+            });
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+
+            services.AddControllers().AddJsonOptions(lbda
+                => lbda.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+            services.AddAutoMapper(typeof(Program));
+
+
+            services.AddDbContext<AppCoreDbContext>(options => options
+                .UseSqlServer(connectionString));
+
+            services.AddDbContext<AppCoreDbContext>(options => options
+                .UseSqlServer(connectionString));
+
+        }
+
+        public static void UseApiConfiguration(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+        }
+    }
+}
--- src/AppCore.API/Program.cs
+++ src/AppCore.API/Program.cs
@@ -1,47 +1,19 @@
 using AppCore.API.Configuration;
-using AppCore.Data.Context;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<AppCoreDbContext>(options => options
-    .UseSqlServer(connectionString));
+builder.AddEnviromentConfiguration();
 
-builder.Services.AddDbContext<AppCoreDbContext>(options => options
-    .UseSqlServer(connectionString));
-
-builder.Services.AddAutoMapper(typeof(Program));
-
-builder.Services.AddControllers().AddJsonOptions(lbda
-    => lbda.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-
-builder.Services.Configure<ApiBehaviorOptions>(lbda =>
-{
-    lbda.SuppressModelStateInvalidFilter = true;
-});
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen();
+builder.Services.AddApiConfiguration(connectionString);
 
 builder.Services.ResolveDependences();
 
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseApiConfiguration();
 
 app.Run();
