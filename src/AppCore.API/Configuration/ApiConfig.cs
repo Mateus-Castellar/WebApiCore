cfb61179a95fa6bb27be0d77347e9dcb184a94fa
Mateus Castellar <mateuscastellar@outlook.com>
2022-08-23 01:16:20 -0300

    feat: Adicionando CORS

--- src/AppCore.API/Configuration/ApiConfig.cs
+++ src/AppCore.API/Configuration/ApiConfig.cs
@@ -24,24 +24,47 @@ namespace AppCore.API.Configuration
             services.AddDbContext<AppCoreDbContext>(options => options
                 .UseSqlServer(connectionString));
 
-            services.AddAutoMapper(typeof(Program));
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Development",
+                    builder =>
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
 
+                options.AddPolicy("Production",
+                    builder =>
+                        builder
+                            .WithMethods("GET")
+                            .WithOrigins("http://apifornecedoresprodutos.api")
+                            .SetIsOriginAllowedToAllowWildcardSubdomains()
+                            //.WithHeaders(HeaderNames.ContentType, "x-custom-header")
+                            .AllowAnyHeader());
+            });
+
+            services.AddAutoMapper(typeof(Program));
         }
 
         public static void UseApiConfiguration(this WebApplication app)
         {
             if (app.Environment.IsDevelopment())
             {
+                app.UseCors("Development");
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            else
+            {
+                app.UseCors("Development"); // Usar apenas nas demos => Configuração Ideal: Production
+                app.UseHsts();//só aceita https (na primeira conexao com https)
+            }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection();//redireciona para https
 
             app.UseAuthorization();
 
             app.MapControllers();
-
         }
     }
 }
