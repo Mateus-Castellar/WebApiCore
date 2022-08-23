93d71bda4c71a61ceba2b4482a2d789df40ece53
Mateus Castellar <mateuscastellar@outlook.com>
2022-08-23 10:31:05 -0300

    feat: Logging nas controller

--- src/AppCore.API/V1/Controllers/AuthController.cs
+++ src/AppCore.API/V1/Controllers/AuthController.cs
@@ -19,13 +19,16 @@ namespace AppCore.API.V1.Controllers
         private readonly SignInManager<IdentityUser> _signInManager;//gerencia a autenticacao
         private readonly UserManager<IdentityUser> _userManager;//gerencia o usuario
         private readonly AppSettings _appSettings;
+        private readonly ILogger _logger;
 
         public AuthController(INotificador notificador, SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager, IOptions<AppSettings> appSettings, IUser user) : base(notificador, user)
+            UserManager<IdentityUser> userManager, IOptions<AppSettings> appSettings, IUser user,
+            ILogger<AuthController> logger) : base(notificador, user)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _appSettings = appSettings.Value;
+            _logger = logger;
         }
 
         [HttpPost("nova-conta")]
@@ -44,6 +47,7 @@ namespace AppCore.API.V1.Controllers
 
             if (result.Succeeded)
             {
+                _logger.LogInformation($"usuário {user.Email} cadastrado com sucesso");
                 await _signInManager.SignInAsync(user, false);
                 return CustomResponse(await GerarJWT(user.Email));
             }
@@ -63,7 +67,10 @@ namespace AppCore.API.V1.Controllers
                 loginUser.Password, false, true);
 
             if (result.Succeeded)
+            {
+                _logger.LogInformation($"usuário {loginUser.Email} cadastrado com sucesso");
                 return CustomResponse(await GerarJWT(loginUser.Email));
+            }
 
             if (result.IsLockedOut)
             {
@@ -128,5 +135,14 @@ namespace AppCore.API.V1.Controllers
             return (long)Math.Round((date.ToUniversalTime() - new
                 DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
         }
+
+        //Tipos de retornos possíveis com log
+
+        //_logger.LogTrace("Log de Trace");
+        //_logger.LogDebug("Log de Debug");
+        //_logger.LogInformation("Log de Informação");
+        //_logger.LogWarning("Log de Aviso");
+        //_logger.LogError("Log de Erro");
+        //_logger.LogCritical("Log de Problema Critico");
     }
 }
